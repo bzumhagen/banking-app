ca969896c028b89556c25e01f4ac63c908476790
Ben Zumhagen <ben.zumhagen@relativity.com>
2021-04-21 21:09:15 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+let accountToTotal = {}
+// accountNumber => { currentBalance, list of audits [{depositAmount, dateTime{}] }
+
+
+express()
+  .use(express.json())
+  .get('/', (req, res) => res.send("Hello world"))
+  .post('/api/deposits', (req, res) =>
+  {
+    const accountNumber = Number(req.body.accountNumber)
+    const depositAmount = Number(req.body.depositAmount)
+    let accountDetails = accountToTotal[accountNumber] || {}
+    let currentBalance = accountDetails.currentBalance || 0
+    let audits = accountDetails.audits || []
+    let currentDate = new Date()
+    let newAudit = {
+      depositAmount: depositAmount,
+      date: currentDate
+    }
+    audits.push(newAudit)
+    accountDetails.currentBalance = currentBalance + depositAmount
+    accountDetails.audits = audits
+    accountToTotal[accountNumber] = accountDetails
+
+    const reponseObject = {
+      newBalance: accountDetails.currentBalance
+    }
+    res.send(reponseObject)
+  })
+  .get('/api/accounts/:accountNumber', (req, res) => {
+    const accountDetails = accountToTotal[req.params.accountNumber] || {}
+    const responseObject = {
+      currentBalance: accountDetails.currentBalance || 0
+    }
+    res.send(responseObject)
+  })
+  .get('/api/accounts/:accountNumber/audits', (req, res) => {
+    const accountDetails = accountToTotal[req.params.accountNumber] || {}
+    res.send(accountDetails.audits)
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
